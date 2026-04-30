562139a227827e02efbbd60a3d5b81db12adc457
Muhammad Khurram Malik <155771526+KhurramMalik15@users.noreply.github.com>
2026-04-30 09:39:05 +0000

    add detail page

---
+++ item_details.js
@@ -0,0 +1,8 @@
+
+const foundBtn = document.getElementById("found-btn");
+    foundBtn.addEventListener("click", () => {
+      alert("Thank you for reporting that you found this item!");
+    });
+
+
+
